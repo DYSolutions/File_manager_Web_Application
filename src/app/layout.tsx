884f1c6a522a7abac982ed0dc71b7aa88b050389
coops15 <worldhidden0@gmail.com>
2025-03-12 02:25:54 +0530

    auth changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import LoginButton from "./components/LoginButton"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <SessionProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><LoginButton />
         {children}
       </body>
     </html>
+    </SessionProvider>
   );
 }
